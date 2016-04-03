c183d39991e6d63be72c5091408d1688890d5e66
Nate Weaver <wevah@derailer.org>
2016-04-03 06:20:14 -0500

    More tests.

--- Example/PunycodeTests/PunycodeTests.m
+++ Example/PunycodeTests/PunycodeTests.m
@@ -73,10 +73,11 @@
 						   @"http://www.bücher.ch/bücher":				@"http://www.xn--bcher-kva.ch/b%C3%BCcher",
 						   @"https://www.google.co.jp/webhp?foo#q=渋谷":	@"https://www.google.co.jp/webhp?foo#q=%E6%B8%8B%E8%B0%B7",
 						   @"http://foo:bar@example.com/":				@"http://foo:bar@example.com/",
-						   @"http://föo:bår@example.com/":				@"http://f%C3%B6o:b%C3%A5r@example.com/"
+						   @"http://föo:bår@example.com/":				@"http://f%C3%B6o:b%C3%A5r@example.com/",
+						   @"http://föo@example.com/":					@"http://f%C3%B6o@example.com/"
 						   };
 	[dict enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
-		XCTAssertTrue([key.encodedURLString isEqualToString:obj], @"%@ should encode to %@", key, obj);
+		XCTAssertTrue([key.encodedURLString isEqualToString:obj], @"%@ should encode to %@; encoded to %@", key, obj, key.encodedURLString);
 	}];
 }
 
@@ -86,7 +87,8 @@
 						   @"http://www.xn--bcher-kva.ch/b%C3%BCcher":					@"http://www.bücher.ch/bücher",
 						   @"https://www.google.co.jp/webhp?foo#q=%E6%B8%8B%E8%B0%B7":	@"https://www.google.co.jp/webhp?foo#q=渋谷",
 						   @"http://foo:bar@example.com/":	@"http://foo:bar@example.com/",
-						   @"http://f%C3%B6o:b%C3%A5r@example.com/": @"http://föo:bår@example.com/"	
+						   @"http://f%C3%B6o:b%C3%A5r@example.com/": @"http://föo:bår@example.com/",
+						   @"http://f%C3%B6o@example.com/": @"http://föo@example.com/"
 						   };
 	[dict enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *obj, BOOL *stop) {
 		XCTAssertTrue([key.decodedURLString isEqualToString:obj], @"%@ should decode to %@; decoded to %@", key, obj, key.decodedURLString);
