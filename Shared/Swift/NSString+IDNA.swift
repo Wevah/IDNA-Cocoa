d0b3929f53c254f2ccb7d80135de468288a2faf0
Nate Weaver <wevah@derailer.org>
2020-04-18 15:33:03 -0500

    Add wrapper functions for Objective-C types

---
+++ Shared/Swift/NSString+IDNA.swift
@@ -0,0 +1,29 @@
+//
+//  NSString+IDNA.swift
+//  PunyCocoa Swift
+//
+//  Created by Nate Weaver on 2020-04-18.
+//
+
+import Foundation
+
+@objc public extension NSString {
+	@objc(IDNAEncoded) var idnaEncoded: String? { (self as String).idnaEncoded }
+	@objc(IDNADecoded) var idnaDecoded: String? { (self as String).idnaDecoded }
+	var encodedURLString: String? { (self as String).encodedURLString}
+	var decodedURLString: String? { (self as String).encodedURLString }
+}
+
+@objc public extension NSURL {
+	@objc(URLWithUnicodeString:) static func urlWithUnicodeString(_ unicodeString: String) -> URL? {
+		return URL(unicodeString: unicodeString)
+	}
+
+	var decodedURLString: String? {
+		return (self as URL).decodedURLString
+	}
+
+	@objc(URLWithUnicodeString:relativeToURL:) static func urlWithUnicodeString(_ unicodeString: String, relativeTo url: URL) -> URL? {
+		return URL(unicodeString: unicodeString, relativeTo: url)
+	}
+}
