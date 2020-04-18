404fb1d5bf143fe3a9b42e00a56afcf6b2dcf477
Nate Weaver <wevah@derailer.org>
2020-04-18 16:43:53 -0500

    Remove loading in AppDelegate

--- Mac (Swift)/AppDelegate.swift
+++ Mac (Swift)/AppDelegate.swift
@@ -12,23 +12,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
 
-
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		guard UTS46.characterMap.isEmpty else { return }
-		if let blobURL = Bundle.main.url(forResource: "uts46", withExtension: "xz") {
-			do {
-				try UTS46.load(from: blobURL, compression: .lzma)
-			} catch {
-				print("error: \(error)")
-			}
-		}
-
-	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
-
-
 }
-
