24bcb4710e18631e30a5f08b194a4896a510ed41
Nate Weaver <wevah@derailer.org>
2020-04-23 14:32:16 -0500

    Skip surrogate codepoints and tweak doc comment

--- Shared/Swift/CharacterSet+Extensions.swift
+++ Shared/Swift/CharacterSet+Extensions.swift
@@ -9,28 +9,39 @@ import Foundation
 
 extension CharacterSet {
 
-	/// Convert a character set to a string with character range pairs of the form
+	/// Convert a character set to UTF-8 data with character range pairs of the form
 	/// `[start][end] ...`
+	///
+	/// Example:
+	/// ```
+	/// let charset = CharacterSet(charactersIn: "abcdefgwxyz")
+	///
+	/// let data = charset.rangeStringData()
+	/// // data is "agwz" (UTF-8 encoded)
+	/// ```
 	func rangeStringData() -> Data {
 		var str = String()
 
 		var startChar: UnicodeScalar?
 		var lastChar: UnicodeScalar?
 
-		for i in 0...0x10FFFF {
-			if let c = UnicodeScalar(i) {
-				if self.contains(c) {
-					if startChar == nil {
-						startChar = c
+		// Skip surrogate codepoints
+		for range in [0...0xD7FF, 0xE000...0x10FFFF] {
+			for i in range {
+				if let c = UnicodeScalar(i) {
+					if self.contains(c) {
+						if startChar == nil {
+							startChar = c
+						}
+
+						lastChar = c
+					} else if let start = startChar, let last = lastChar {
+						str.unicodeScalars.append(start)
+						str.unicodeScalars.append(last)
+
+						startChar = nil
+						lastChar = nil
 					}
-
-					lastChar = c
-				} else if let start = startChar, let last = lastChar {
-					str.unicodeScalars.append(start)
-					str.unicodeScalars.append(last)
-
-					startChar = nil
-					lastChar = nil
 				}
 			}
 		}
