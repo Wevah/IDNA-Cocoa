71da66d09dce1026b09187c3f8f6e22aca5e2589
Nate Weaver <wevah@derailer.org>
2020-05-08 17:42:00 -0500

    Move file format doc comment to UTS46.swift

--- icumap2code/main.swift
+++ icumap2code/main.swift
@@ -8,64 +8,6 @@
 import Foundation
 import ArgumentParser
 
-/// Output file format. Codepoints are stored UTF-8-encoded.
-///
-/// All multibyte integers are little-endian.
-///
-/// Header:
-///
-///     +--------------+---------+---------+---------+
-///     | 6 bytes      | 1 byte  | 1 byte  | 4 bytes |
-///     +--------------+---------+---------+---------+
-/// 	| magic number | version | flags   | crc32   |
-///     +--------------+---------+---------+---------+
-///
-/// - `magic number`: `"UTS#46"` (`0x55 0x54 0x53 0x23 0x34 0x36`).
-/// - `version`: format version (1 byte; currently `0x01`).
-/// - `flags`: Bitfield:
-///
-///       +-----+-----+-----+-----+-----+-----+-----+-----+
-///       |  7  |  6  |  5  |  4  |  3  |  2  |  1  |  0  |
-///       +-----+-----+-----+-----+-----+-----+-----+-----+
-///       | currently unused      | crc | compression     |
-///       +-----+-----+-----+-----+-----+-----+-----+-----+
-///
-///     - `crc`: Contains a CRC32 of the data after the header.
-///     - `compression`: compression mode of the data.
-///       Currently identical to NSData's compression constants + 1:
-///
-///       - 0: no compression
-///       - 1: LZFSE
-///       - 2: LZ4
-///       - 3: LZMA
-///       - 4: ZLIB
-///
-/// - `crc32`: CRC32 of the (possibly compressed) data. Implementations can skip
-///   parsing this unless data integrity is an issue.
-///
-/// The data section is a collection of data blocks of the format
-///
-///     [marker][section data] ...
-///
-/// Section data formats:
-///
-///	If marker is `characterMap`:
-///
-///	    [codepoint][mapped-codepoint ...][null] ...
-///
-///	If marker is `disallowedCharacters` or `ignoredCharacters`:
-///
-///	    [codepoint-range] ...
-///
-///	If marker is `joiningTypes`:
-///
-///		[type][[codepoint-range] ...]
-///
-///	where `type` is one of `C`, `D`, `L`, `R`, or `T`.
-///
-///	`codepoint-range`: two codepoints, marking the first and last codepoints of a
-///	closed range. Single-codepoint ranges have the same start and end codepoint.
-///
 struct ICUMap2Code: ParsableCommand {
 	static let configuration = CommandConfiguration(commandName: "icumap2code", abstract: "Convert UTS#46 and joiner type map files to a compact binary format.", version: "1.0 (v10)")
 
