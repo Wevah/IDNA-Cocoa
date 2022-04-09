40813d4c24fe114c3c5063e03b0adf931445c8ec
Nate Weaver <wevah@derailer.org>
2022-04-08 23:51:53 -0500

    Use crc32_z on macOS 10.13+ and iOS 11.0+

--- Sources/IDNA/Data+Extensions.swift
+++ Sources/IDNA/Data+Extensions.swift
@@ -10,11 +10,21 @@ import zlib
 
 extension Data {
 
+	/// Compute the CRC-32 of the data.
+	///
+	/// Does not handle data with a `count` larger than `UInt32.max` on macOS
+	/// < 10.13 or on iOS < 11.0.
 	var crc32: UInt32 {
 		return self.withUnsafeBytes {
 			let buffer = $0.bindMemory(to: UInt8.self)
-			let initial = zlib.crc32(0, nil, 0)
-			return UInt32(zlib.crc32(initial, buffer.baseAddress, numericCast(buffer.count)))
+
+			if #available(macOS 10.13, iOS 11.0, tvOS 9999.0, watchOS 9999.0, *) {
+				let initial = zlib.crc32_z(0, nil, 0)
+				return UInt32(zlib.crc32_z(initial, buffer.baseAddress, buffer.count))
+			} else {
+				let initial = zlib.crc32(0, nil, 0)
+				return UInt32(zlib.crc32(initial, buffer.baseAddress, UInt32(buffer.count)))
+			}
 		}
 	}
 
